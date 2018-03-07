3b6b0c4de47fb1937145fa505531413a0fe3df10
Eric McKinney <mckinney99@gmail.com>
2018-03-07 17:17:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+function showRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText)
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
 
-function showRepositories(event, data) {
-  //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText)
-}
 
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
