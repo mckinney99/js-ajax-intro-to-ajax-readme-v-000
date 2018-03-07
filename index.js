224d5c7646f69234da5b0b77cb0a304aeb6e97cf
Eric McKinney <mckinney99@gmail.com>
2018-03-07 17:16:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ function showRepositories(event, data) {
   repoList += "</ul>"
   document.getElementById("repositories").innerHTML = repoList
 }
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
