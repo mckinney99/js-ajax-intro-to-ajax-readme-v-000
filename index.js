4452b679fc401464e0e213ed61110da22b6063d2
Eric McKinney <mckinney99@gmail.com>
2018-03-07 17:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText)
 }
+
+function showRepositories(event, data) {
+  console.log(this.responseText)
+  let repoList = "<ul>"
+  for(var i=0;i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
+}
